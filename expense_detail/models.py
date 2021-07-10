d7ec80cac4fb4bf03330826e991dbc548d6d970b
Het <hetmodi48@gmail.com>
2021-07-10 16:18:38 +0530

    expense_detail is completed!!!

--- expense_detail/models.py
+++ expense_detail/models.py
@@ -5,13 +5,20 @@ from django.urls import reverse
 
 class expense(models.Model):
     date=models.DateField(default=datetime.utcnow)
-    tea_coffee=models.IntegerField()
-    electricity=models.IntegerField()
-    water_bill=models.IntegerField()
-    stationary=models.IntegerField()
-    labour_expense=models.IntegerField()
-    property_tax=models.IntegerField()
-    remark=models.TextField(max_length=100)
+    tea_coffee=models.IntegerField(null=True,blank=True)
+    electricity=models.IntegerField(null=True,blank=True)
+    water_bill=models.IntegerField(null=True,blank=True)
+    stationary=models.IntegerField(null=True,blank=True)
+    labour_expense=models.IntegerField(null=True,blank=True)
+    property_tax=models.IntegerField(null=True,blank=True)
+    maintainence_expense=models.IntegerField(null=True,blank=True)
+    machinory_expense=models.IntegerField(null=True,blank=True)
+    insurence_expence=models.IntegerField(null=True,blank=True)
+    ruff_expense=models.IntegerField(null=True,blank=True)
+    employee_insurance_expense=models.IntegerField(null=True,blank=True)
+    total_Amount = models.IntegerField(null=True,blank=True)
+    remark=models.TextField(max_length=50,null=True,blank=True)
+
 
     def __str__(self):
         return f"{self.date}"
