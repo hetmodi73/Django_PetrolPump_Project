7e669f6669f1aab1e3053ca9795733e3caaef600
Het <hetmodi48@gmail.com>
2021-07-17 23:23:57 +0530

    Edited modules & username with photo is added!!!

--- employee_master/models.py
+++ employee_master/models.py
@@ -36,7 +36,7 @@ class employee(models.Model):
     education=models.CharField(max_length=20)
     Date_of_join=models.DateField()
     expected_salary=models.IntegerField(validators=[MinValueValidator(0,'Value should not be less than 0')])
-    photo=models.ImageField(default='default.jpg',upload_to='profile')
+    photo=models.ImageField(default='default.jpg',upload_to='profile',blank=True,null=True)
     Document_file = models.FileField(blank=True,null=True,upload_to='profile')
     bank_name=models.CharField(max_length=20,null=True,blank=True)
     account_no=models.IntegerField(blank=True,null=True,validators=[MinValueValidator(0,'Value should not be less than 0')])
